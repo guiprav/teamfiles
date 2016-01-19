f238cd26529e82e8f0c81c7ca9abb3b710496364
Guilherme Vieira <guilherme.vieira@wmg.com>
2016-01-19 09:27:56 -0200

    Implement upload form.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var fs = require('fs');
+var readTextFileSync = require('read-text-file-sync');
 var path = require('path');
 var extname = path.extname;
 var basename = path.basename;
@@ -9,6 +10,7 @@ var app = express();
 var uploadsPath = 'uploads';
 var multer = require('multer');
 var upload = multer({ dest: uploadsPath });
+var uploadPage = readTextFileSync(__dirname + '/upload.html');
 var key = process.env.TEAMKEY;
 
 if(!key) {
@@ -16,14 +18,32 @@ if(!key) {
     process.exit(-1);
 }
 
+function keyCheck(req, res, next) {
+    if(req.params.key !== key) {
+        res.status(403);
+        res.send("Bad team key.");
+        return;
+    }
+
+    next();
+}
+
+app.get('/upload/:key', keyCheck, function(req, res) {
+    res.send(uploadPage);
+});
+
+app.post('/upload/:key', keyCheck, upload.single('file'), function(req, res) {
+    var originalPath = uploadsPath + '/' + req.file.filename;
+    var newPath = originalPath + extname(req.file.originalname);
+
+    fs.renameSync(originalPath, newPath);
+
+    res.send(basename(newPath) + '/' + req.file.originalname);
+});
+
 app.use(function(req, res, next) {
     var urlPath = req.url;
     var reResults;
-
-    if(req.method === 'POST') {
-        next();
-        return;
-    }
 
     reResults = /^(\/[^\/]+)/.exec(urlPath);
 
@@ -38,24 +58,5 @@ app.use(function(req, res, next) {
 });
 
 app.use(express.static(uploadsPath));
-
-function keyCheck(req, res, next) {
-    if(req.params.key !== key) {
-        res.status(403);
-        res.send("Bad team key.");
-        return;
-    }
-
-    next();
-}
-
-app.post('/upload/:key', keyCheck, upload.single('file'), function(req, res) {
-    var originalPath = uploadsPath + '/' + req.file.filename;
-    var newPath = originalPath + extname(req.file.originalname);
-
-    fs.renameSync(originalPath, newPath);
-
-    res.send(basename(newPath) + '/' + req.file.originalname);
-});
 
 app.listen(process.env.PORT || 3000);
